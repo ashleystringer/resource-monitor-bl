9adb27f729fa796c4299ed8e7648eee38150093d
ashleystringer <ashstrin@umail.iu.edu>
2021-03-06 22:16:42 -0500

    message

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -9,20 +9,27 @@ const {
  */
 module.exports = ResourceController.extend ({
   name: 'file',
-  model: model("person"),
+  model: model("call"),
   init(){
     console.log('Initialized');
   },
   getAll(){
     return Action.extend({
       execute(req, res){
+        var data;
         console.log('execute');
-        this.controller.model.find((err)=>{
+        this.controller.model.find((err, file)=>{
             if(err){
               console.log('An error occurred: ' + err);
+            }else{
+              console.log('Not error ');
+              //console.log(file);
+              console.log(typeof(file));
             }
-            res.status(200);
+            //console.log(data);
         });
+        console.log(data);
+        res.status(200).json({data});
       }
     });
   }
